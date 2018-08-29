83d637133fd5a7e1a4ceecd4cd1095b6c5d48f6e
vipulrawat <vipulrawat.imi@live.in>
2018-08-29 17:48:58 +0530

    ERN boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        No display
+      </div>
+    );
+  }
+}
+
+export default App;
