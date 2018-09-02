266d6d120814acf4f6bbfb39445219d56f6da146
vipulrawat <vipulrawat.imi@live.in>
2018-09-02 20:04:05 +0530

    CSS touch

--- client/src/components/Loading.js
+++ client/src/components/Loading.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import spinner from './images/spinner.gif';
 
 export default () => {
   return (
-    <div>
-      <img
-        src={spinner}
-        alt="Loading..."
-        style={{ width: '200px', margin: ' 40px auto', display: 'block' }}
-      />
-    </div>
+    <div className="loader">Loading...</div>
   );
 };
--- client/src/components/Weather.js
+++ client/src/components/Weather.js
@@ -4,20 +4,7 @@ import axios from 'axios'
 import Icon from './Icon';
 import Loading from './Loading';
 import Error from './Error';
-//import rain from './rain.png';
-/*
-import rain from './images/rain.png';
-import clearday from './images/clear-day.png';
-import clearnight from './images/clear-night.png';
-import snow from './images/snow.png';
-import sleet from './images/sleet.png';
-import wind from './images/wind.png';
-import fog from './images/fog.png';
-import cloudy from './images/cloudy.png';
-import partlycloudyday from './images/partlycloudyday.png';
-import partlycloudynight from './images/partlycloudynight.png';
-import alert from './images/alert.png';
-*/
+
 class Weather extends Component {
     constructor(props){
         super(props)
@@ -40,8 +27,6 @@ class Weather extends Component {
               self.setState({haveData:true,isLoading:false,weatherData:data});
               let str = data[0].icon;
               self.setState({icon:str.split('-').join('')})
-              console.log('ICON:'+self.state.icon);
-              console.log(self.state.weatherData)
           })
           .catch(function (error) {
               if(error.response){
@@ -67,7 +52,7 @@ class Weather extends Component {
     else if(this.state.haveData===false && this.state.isLoading===false){    
         return(
             <div>
-                <h3>No Location is set</h3>
+                <h3>No Location is set. Please enter the location below.</h3>
             </div>
         );
     } 
@@ -82,8 +67,8 @@ class Weather extends Component {
         return(
             <div className="weather">
                 <div id="icon"><Icon ic={this.state.icon}/></div>
-                <div id="temperature">{this.state.weatherData[0].temperature}
-                    <span>{this.state.weatherData[0].summary}</span>
+                <div id="temperature">{Math.round(this.state.weatherData[0].temperature)}
+                   <sup>&#176;C</sup> <span>{this.state.weatherData[0].summary}</span>
                 </div>
                 <div id="location">{this.state.weatherData[1]}</div>
             </div>
